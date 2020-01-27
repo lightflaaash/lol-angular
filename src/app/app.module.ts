c094e34361fa58fb283c798b7f8ba2ff0ce0d333
Kabz Aymane <aymanekabz@gmail.com>
2020-01-27 16:35:59 +0100

    vv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChampionsComponent } from './champions/champions.component';
+import { ChampionComponent } from './champions/champion/champion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionsComponent,
+    ChampionComponent
   ],
   imports: [
     BrowserModule,
